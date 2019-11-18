7efacb0fb02fd12ce06fe3f2a2a6419f9b72ba2d
Quzzar <quzzar@RonaldBook.localdomain>
2019-11-18 00:42:50 -0800

    first commit

--- lab08/partB/header.h
+++
@@ -1,7 +0,0 @@
-#ifndef _HEADER_H_
-#define _HEADER_H_
-
-int testNumber(int test, int max);
-int inputInt();
-
-#endif
--- lab08/partC/header.h
+++
@@ -1,9 +0,0 @@
-#ifndef _HEADER_H_
-#define _HEADER_H_
-
-float getCostToFill(float pricePerGallon, int tankCapacity);
-void printStats(float pricePerGallon, int tankCapacity, float costToFill);
-void setCostToFill(float pricePerGallon, int tankCapacity, float *costToFill);
-void inputPriceAndTank(float *pricePerGallon, int *tankCapacity);
-
-#endif
